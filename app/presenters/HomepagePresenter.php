8c4447850c49b6e7c80dbc6b54ebb3abdf211b15
TomasVotruba <tomas.vot@gmail.com>
2017-03-01 01:03:42 +0100

    HomepagePresenter: drop dependency on parent helper method

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -5,16 +5,28 @@ namespace App\Presenters;
 use Nette\Application\IResponse;
 use Nette\Application\Request;
 use Nette\Application\Responses\TextResponse;
+use Nette\Application\UI\ITemplateFactory;
 use Nette\Application\UI\Presenter;
+use Nette\Bridges\ApplicationLatte\TemplateFactory;
 
 
 final class HomepagePresenter extends Presenter
 {
 
+	/**
+	 * @var TemplateFactory
+	 */
+	private $templateFactory;
+
+
+	public function __construct(ITemplateFactory $templateFactory)
+	{
+		$this->templateFactory = $templateFactory;
+	}
+
 	public function run(Request $request): IResponse
 	{
-		$template = $this->getTemplateFactory()
-			->createTemplate($this);
+		$template = $this->templateFactory->createTemplate($this);
 		$template->setFile(__DIR__ . '/templates/Homepage/default.latte');
 
 		return new TextResponse($template);
