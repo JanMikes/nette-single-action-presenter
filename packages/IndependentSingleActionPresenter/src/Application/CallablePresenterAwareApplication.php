728345b017cc1e9895c0376c07925f3fffa3bfa3
TomasVotruba <tomas.vot@gmail.com>
2017-03-04 23:02:27 +0100

    move code to package

--- packages/IndependentSingleActionPresenter/src/Application/CallablePresenterAwareApplication.php
+++ packages/IndependentSingleActionPresenter/src/Application/CallablePresenterAwareApplication.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace SingleActionPresenter\Application;
+namespace IndependentSingleActionPresenter\Application;
 
 use Nette;
 use Nette\Application\Application;
